9a58ebf813d4e69048f77d319f01fde6c8143297
rfarq <richard.farquharson@gmail.com>
2018-10-07 17:26:41 +0100

    Define remote modules

--- js/main.js
+++ js/main.js
@@ -4,36 +4,6 @@ require.config({
     }
 });
 
-define("taskData", [], function() {
-    "use strict";
-
-    /* load and save data */
-
-    var STORE_NAME = "tasks";
-
-    function saveTaskData (tasks) {
-        localStorage.setItem(STORE_NAME, JSON.stringify(tasks));
-    }
-
-    function loadTaskData () {
-        var storedTasks = localStorage.getItem(STORE_NAME);
-        if(storedTasks) {
-            return JSON.parse(storedTasks);
-        }
-        return [];
-    }
-
-    function clearTaskData () {
-        localStorage.removeItem(STORE_NAME);
-    }
-
-    return {
-        save: saveTaskData,
-        load: loadTaskData,
-        clear: clearTaskData
-    };
-});
-
 define("taskRenderer", ["jquery"], function ($) {
     /* create DOM task elements */
 
